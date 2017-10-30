f26bf7a60fc0fc16afa65d37be84ffcf44fbf63a
ralphsu <RalphSu@users.noreply.github.com>
2017-10-30 10:02:28 -0500

    Update 1.bash

--- 1.bash
+++ 1.bash
@@ -1,4 +1,5 @@
 #! /bin/bash
 java -javaagent:/ebay/repositories/btrace/build/btrace-agent.jar com.ebay.btrace.Main
 
-# JAVA SecurityException might happen.
+# JAVA SecurityException might happen. It's possibly that caused by btrace-boot.jar not copied. Make sure you have btrace-boot.jar in classpath.
+# 
